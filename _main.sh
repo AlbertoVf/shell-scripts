c566a9e2f92bf92c671ce004a302694dfc0bb58a
AlbertoVf <avf.git-repos@protonmail.com>
2024-03-31 00:09:26 +0100

    refactor(yt): merge yt script
    merge script to use only file to download audio/video, playlist or file

--- _main.sh
+++ _main.sh
@@ -7,23 +7,29 @@ _SUCCESS="\033[1m \033[32m"
 _WARNING="\033[1m \033[33m"
 _LOG="\033[1m \033[36m"
 
+video_regex='^https?:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,}(\/\S*)?$'
+playlist_regex='^(https?:\/\/)?(www\.)?(youtube\.com\/|youtu\.be\/)(playlist\?|list=)([^#\&\?\n]+)'
+
 _log() {
 	echo -e "$_LOG[LOG] $1 $_RESET"
 }
 
 _success() {
 	echo -e "$_SUCCESS[SUCCESS] $1 $_RESET"
+	paplay '/usr/share/sounds/freedesktop/stereo/complete.oga'
 }
 
 _error() {
 	echo -e "$_ERROR[ERROR] $1 $_RESET"
+	paplay '/usr/share/sounds/freedesktop/stereo/dialog-error.oga'
 }
 
 _warning() {
 	echo -e "$_WARNING[WARNING] $1 $_RESET"
+	paplay '/usr/share/sounds/freedesktop/stereo/dialog-warning.oga'
 }
 
-_params_required(){
+_params_required() {
 	usage=$(sed -n '3p' "$0" | cut -c 5-)
 	_error "The script require $1 params\n\t $_WARNING$usage"
 }
@@ -32,5 +38,5 @@ notify() {
 	local app=$1
 	local mensaje=$2
 	command -v notify-send >/dev/null && notify-send "$app" "$mensaje"
-	paplay '/usr/share/sounds/freedesktop/stereo/complete.oga'
+	paplay '/usr/share/sounds/freedesktop/stereo/message.oga'
 }
