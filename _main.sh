a86088fd7987d6bf7da8b70067ecbe8580975a2d
AlbertoVf <avf.git-repos@protonmail.com>
2025-11-15 13:01:43 +0100

    feat: add metadata extraction script and improve file renaming logic

--- _main.sh
+++ _main.sh
@@ -1,41 +1,41 @@
 #!/usr/bin/env sh
 
 save_log(){
-    local level="$1"; shift
-    echo "[$(date '+%Y-%m-%d %H:%M:%S')] [$level] $*" >> "$HOME/.logs/$(basename "$0").log"
+	local level="$1"; shift
+	echo "[$(date '+%Y-%m-%d %H:%M:%S')] [$level] $*" >> "$HOME/.logs/$(basename "$0").log"
 }
 
 emit_message() {
-    local sound="$1";shift
+	local sound="$1";shift
 	local status="$1"; shift
-    local message="$1"
+	local message="$1"
 	[ -t 1 ] && echo -e "$status $message \033[0m"
-    notify-send "$message"
-    paplay "/usr/share/sounds/freedesktop/stereo/$sound.oga"
+	notify-send "$message"
+	paplay "/usr/share/sounds/freedesktop/stereo/$sound.oga"
 }
 
 _log() {
-    _DEBUG="\033[1m\033[30m [*] $(date +'%H:%M:%S') »"
+	_DEBUG="\033[1m\033[30m [*] $(date +'%H:%M:%S') »"
 	emit_message 'message' "$_DEBUG" "$1"
-    save_log "DEBUG" "$1"
+	save_log "DEBUG" "$1"
 }
 
 _success() {
-    _INFO="\033[1m\033[32m [+] $(date +'%H:%M:%S') »"
+	_INFO="\033[1m\033[32m [+] $(date +'%H:%M:%S') »"
 	emit_message 'complete' "$_INFO" "$1"
-    save_log "INFO" "$1"
+	save_log "INFO" "$1"
 }
 
 _error() {
-    _ERROR="\033[1m\033[31m [-] $(date +'%H:%M:%S') »"
+	_ERROR="\033[1m\033[31m [-] $(date +'%H:%M:%S') »"
 	emit_message 'dialog-error' "$_ERROR" "$1"
-    save_log "ERROR" "$1"
+	save_log "ERROR" "$1"
 }
 
 _warning() {
-    _WARNING="\033[1m\033[33m [!] $(date +'%H:%M:%S') »"
+	_WARNING="\033[1m\033[33m [!] $(date +'%H:%M:%S') »"
 	emit_message 'dialog-warning' "$_WARNING" "$1"
-    save_log "WARNING" "$1"
+	save_log "ALERT" "$1"
 }
 
 _params_required() {
